79ba555a2c6581dc43bebb1e2736e33702d32f8a
Jayshri <jayshrimunde97@gmail.com>
2024-02-28 21:36:45 +0530

    stopwatch

---
+++ src/StopWatch/Stopwatch.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from "react";
+import "./Stopwatch.css";
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, SetIsRunning] = useState(false);
+  const intervalRef = useRef(null);
+
+  const start = () => {
+    if (!isRunning) {
+      SetIsRunning(true);
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    }
+  };
+
+  const reset = () => {
+    clearInterval(intervalRef.current);
+    SetIsRunning(false);
+    setTime(0);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    // const milliseconds = Math.floor((time % 1000) / 10);
+
+    return (
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds
+      // +
+      // // ":" +
+      // // (milliseconds < 10 ? "0" : "") +
+      // // milliseconds
+    );
+  };
+
+  return (
+    <div className="stopwatch">
+      <h1>Stopwatch</h1>
+      <p>Time:{formatTime(time)}</p>
+      <div>
+        <button onClick={start}>Start</button>
+        <button onClick={reset}>Reset </button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
